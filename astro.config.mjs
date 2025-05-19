edd0616cf8e82f25a4db912ef9e51788e3e15a94
Geoffrey-COUTANT <geoffrey22.coutant@outlook.fr>
2025-05-19 20:32:20 +0200

    add fixed probles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-const isProd = process.env.NODE_ENV === 'production';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    site: 'https://geoffrey-coutant.github.io/portfolio/',
-    base: isProd ? '/portfolio/' : '/',
     devToolbar: {
         enabled: false
     },
     alias: {
         '@': './src'
     },
+    output: 'static',
     integrations: [react(), tailwind(), sitemap()]
 });
