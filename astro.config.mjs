26c24ac4b666121dd4cbe83995145152433e5353
Geoffrey-COUTANT <geoffrey22.coutant@outlook.fr>
2025-05-19 18:36:30 +0200

    add new lines

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://geoffrey-coutant.github.io/portfolio/',
+    base: '/',
     devToolbar: {
         enabled: false
     },
